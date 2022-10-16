49aaedcef0f0df8d718a38911a2f3057c3f74f0c
vudu79 <33384007+vudu79@users.noreply.github.com>
2022-10-17 01:26:01 +1200

    Update client.py

--- handlers/client.py
+++ handlers/client.py
@@ -7,8 +7,8 @@ from create_bot import dp, bot
 @dp.message_handler(commands=['start', 'help'])
 async def start_handler(message : types.Message):
     await bot.send_message(message.from_user.id, f'Привет, {message.from_user.first_name}!')
-    await bot.send_message(message.from_user.id, f'Привет, {message.from_user.first_name}! Это бот для поиска гифок в интернете разными способами).', reply_markup=reply_keyboard)
-    await bot.send_message(message.from_user.id, "Что будем искать?)")
+    await bot.send_message(message.from_user.id, 'Это бот для поиска гифок в интернете разными способами))', reply_markup=reply_keyboard)
+    await bot.send_message(message.from_user.id, "Как будем искать? тыц 👇")
 
 @dp.message_handler(commands=['Выборка'])
 async def start_handler(message : types.Message):
