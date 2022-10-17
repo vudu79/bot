f1ba7ce1e6b9c08a54d6a3594682c7d1c7d30e37
Your Name <you@example.com>
2022-10-18 01:31:39 +1200

    refactor server13

--- echo_server/server.py
+++ echo_server/server.py
@@ -8,7 +8,7 @@ class HttpGetHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        # self.send_header("Content-type", "text/html")
+        self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(b'Hello, world!')
 
