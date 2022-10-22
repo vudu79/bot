117cf2c3bc54487aea95317b0f9fd37ff1fd9332
Your Name <you@example.com>
2022-10-22 22:20:09 +1200

    готовлю для переноса на хост

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from echo_server import run, HttpGetHandler, HTTPServer
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_HOST = 'http://tibidoh.ru'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
