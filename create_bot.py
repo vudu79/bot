c0878a13c760a960eb71996e3174ca968aad1dbe
Your Name <you@example.com>
2022-11-17 20:39:21 +0200

    1

--- create_bot.py
+++ create_bot.py
@@ -5,8 +5,8 @@ from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import os
 
-
-TOKEN=os.getenv("BOT_TOKEN")
+# TOKEN = os.getenv("TOKEN")
+TOKEN = "5788022696:AAG6Vw4Feolg4LPQybsU0iAUUaE_UqhwwtQ"
 
 storage = MemoryStorage()
 bot = Bot(token=TOKEN)
@@ -16,7 +16,6 @@ with open('calendar.json', 'r', encoding='utf-8') as f:
     js = f.read()
 
 calendar_dict = json.loads(js)
-
 
 with open('calendar_storage.json', 'r', encoding='utf-8') as f:
     js = f.read()
--- main.py
+++ main.py
@@ -1,69 +1,93 @@
+import asyncio
 import json
 import logging, os
 import multiprocessing
+import pathlib
+
 from handlers import client, admin, other
 from aiogram.utils.executor import start_webhook
 from create_bot import TOKEN, bot, dp
 from echo_server import run, HttpGetHandler, HTTPServer
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
-
-# webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-# webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 
-async def on_shutdown(dispatcher):
-    await bot.delete_webhook()
+#
+# if __name__ == '__main__':
+#     logging.basicConfig(level=logging.INFO)
+#     dp.start_polling()
 
 
-client.register_handlers_client(dp)
-admin.register_handlers_admin(dp)
-other.register_handlers_other(dp)
+
+async def bot_start(logger: logging.Logger) -> None:
+    logging.basicConfig(level=logging.DEBUG)
+
+    # commands_for_bot = []
+    # for cmd in bot_commands:
+    #     commands_for_bot.append(BotCommand(command=cmd[0], description=cmd[1]))
+    #
+    # redis = Redis(
+    #     host=os.getenv('REDIS_HOST') or '127.0.0.1',
+    #     password=os.getenv('REDIS_PASSWORD') or None,
+    #     username=os.getenv('REDIS_USER') or None,
+    # )
+    #
+    # i18n = I18n(path=WORKDIR / 'locales', default_locale='ru', domain='messages')
+    # i18n_middleware = ConstI18nMiddleware(i18n=i18n, locale='ru')
+    #
+    # dp = Dispatcher()
+    # dp.message.middleware(RegisterCheck())
+    # dp.callback_query.middleware(RegisterCheck())
+    #
+    # i18n_middleware.setup(dp)
+    #
+    # bot = Bot(token=os.getenv('token'), parse_mode='HTML')  # type: ignore
+    # await bot.set_my_commands(commands=commands_for_bot)
+    # register_user_commands(dp)
+
+    client.register_handlers_client(dp)
+    admin.register_handlers_admin(dp)
+    other.register_handlers_other(dp)
+
+    # postgres_url = URL.create(
+    #     "postgresql+asyncpg",
+    #     username=os.getenv("POSTGRES_USER"),
+    #     host=os.getenv('POSTGRES_HOST'),
+    #     database=os.getenv("POSTGRES_DB"),
+    #     port=int(os.getenv("POSTGRES_PORT") or 0),
+    #     password=os.getenv('POSTGRES_PASSWORD')
+    # )
+    # async_engine = create_async_engine(postgres_url)
+    # session_maker = get_session_maker(async_engine)
+
+    # Делегировано alembic
+    # await proceed_schemas(async_engine, BaseModel.metadata)
+    # await dp.start_polling(bot, session_maker=session_maker, logger=logger, redis=redis)
+    await dp.start_polling(bot)
+
+
+def setup_env():
+    """Настройка переменных окружения"""
+    from dotenv import load_dotenv
+    path = pathlib.Path(__file__).parent.parent
+    dotenv_path = path.joinpath('.env')
+    if dotenv_path.exists():
+        load_dotenv(dotenv_path)
+
+
+def main():
+    """Функция для запуска через poetry"""
+    logger = logging.getLogger(__name__)
+    try:
+        setup_env()
+        asyncio.run(bot_start(logger))
+        logger.info('Bot started')
+    except (KeyboardInterrupt, SystemExit):
+        logger.info('Bot stopped')
+
 
 if __name__ == '__main__':
+    main()
 
 
-    logging.basicConfig(level=logging.INFO)
-
-    #
-    # start_webhook(
-    #     dispatcher=dp,
-    #     webhook_path=WEBHOOK_PATH,
-    #     skip_updates=True,
-    #     on_startup=on_startup,
-    #     on_shutdown=on_shutdown,
-    #     host=WEBAPP_HOST,
-    #     port=WEBAPP_PORT,
-    # )
-
-    kwa1 = {
-        "dispatcher": dp,
-        "webhook_path": WEBHOOK_PATH,
-        "skip_updates": True,
-        "on_startup": on_startup,
-        "on_shutdown": on_shutdown,
-        "host": WEBAPP_HOST,
-        "port": WEBAPP_PORT,
-    }
-
-    kwa2 = {
-        "server_class" : HTTPServer,
-        "handler_class": HttpGetHandler,
-    }
-
-    p1 = multiprocessing.Process(target=start_webhook, kwargs=kwa1)
-    p2 = multiprocessing.Process(target=run, kwargs=kwa2)
-    p1.start()
-    p2.start()
-
-# asdfsdfsdfsdfsdfsdf
