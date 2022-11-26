f9fbffd8d30cc0e0cb9455e824add5c47d1de570
Your Name <you@example.com>
2022-11-27 00:37:24 +0200

    рефактор вывод рандом стикреров1

--- handlers/admin.py
+++ handlers/admin.py
@@ -123,15 +123,16 @@ async def load_count_random_stickers(message: types.Message, state: FSMContext):
                 else:
                     for x in range(0, 9):
                         media.attach_photo(types.InputMediaPhoto(img_list[x]))
-
-                await bot.send_message(message.from_user.id, f'"{pack["name"]}"')
-                await bot.send_media_group(message.from_user.id, media=media)
-                await bot.send_message(message.from_user.id, "Если зашло, то можно .. ",
+                if len(media.values) > 0:
+                    await bot.send_message(message.from_user.id, f'"{pack["name"]}"')
+                    await bot.send_media_group(message.from_user.id, media=media)
+                    await bot.send_message(message.from_user.id, "Если зашло, то можно .. ",
                                            reply_markup=InlineKeyboardMarkup(row_width=1).add(InlineKeyboardButton(
                                                text=".. добавить в телеграм", url=f'{pack["url"]}')))
             except RetryAfter as e:
                 await asyncio.sleep(e.timeout)
             except Exception as ee:
+                await bot.send_message(message.from_user.id, "Что то пошло не так...")
                 print(f'Что то пошло не так - {ee}')
 
     await state.finish()
